7461cf66c500a4095c575664a1564a4e73204a61
Haris_Ali <siddiquiharis92@gmail.com>
2025-10-17 16:53:39 +0530

    fixes the problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,22 @@
-
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-<<<<<<< HEAD
-  enum role: {public_user: "public", student: "student", admin: "admin"}
-=======
-  enum role: { public_user: "public_user", school_admin: "school_admin", student: "student" }
+  # ✅ Single combined enum
+  enum role: {
+    public_user: "public_user",
+    student: "student",
+    admin: "admin",
+    school_admin: "school_admin"
+  }
 
   after_initialize :set_default_role, if: :new_record?
+
+  private
 
   def set_default_role
     self.role ||= "public_user"
   end
->>>>>>> public-signup-login
 end
