99dcff80d24198b25b422cfeaa0ba38bf2988af0
Ashwani Singh <ashwaniuit07@gmail.com>
2025-10-29 12:17:03 +0530

    Save local changes before merging admin_form

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2025_10_17_061119) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2025_10_17_064053) do
->>>>>>> public-signup-login
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,11 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_17_064053) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
-    t.string "role"
-=======
     t.string "role", default: "public"
->>>>>>> public-signup-login
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
